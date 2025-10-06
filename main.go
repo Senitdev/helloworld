d0eedb44c7dba319ba42e5b1ffdb30a4a95ae585
Senitdev <75537054+Senitdev@users.noreply.github.com>
2025-10-06 15:33:56 +0000

    Test workflow

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	http.HandleFunc("/", HelloHandler)
+	http.HandleFunc("/test", TestWorkFlow)
 	fmt.Printf("Serveur running at port 8090")
 	http.ListenAndServe(":8090", nil)
 }
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello word  from Dockers")
+}
+func TestWorkFlow(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "je test le workflow")
 }
